ebc3232c442225557e3d5f002bb4dc55e6bdc142
oasisinheart <oasisinheart@naver.com>
2018-02-14 14:52:07 +0900

    change

--- web.js
+++ web.js
@@ -11,9 +11,11 @@ app.get('/', function(req, res){
 app.get('/inputdata', function(req, res){
 	res.send('input data!!!');
 });
+
 app.get('/log', function(req, res){
 	res.send('Log Show');
 });
+
 app.listen(8001,function(){
 	console.log('Connect 8001 Port!!!');
 });
