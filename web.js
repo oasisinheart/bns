b5afdba4ee9da26b60bc861e2c03b37b8ff1b0a8
oasisinheart <oasisinheart@naver.com>
2018-02-14 14:48:32 +0900

    Initial commit

---
+++ web.js
@@ -0,0 +1,19 @@
+/**
+ * @author haansoo
+ */
+var express = require('express'); 
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('Hellow World!!');
+});
+
+app.get('/inputdata', function(req, res){
+	res.send('input data!!!');
+});
+app.get('/log', function(req, res){
+	res.send('Log Show');
+});
+app.listen(8001,function(){
+	console.log('Connect 8001 Port!!!');
+});
